808ceb09cabae195ba8b556a7f40fb192d3afe0d
KamenSentai <cvt.alain@gmail.com>
2019-05-27 16:22:42 +0200

    feat(templating): build pages

--- portfolio/views.py
+++ portfolio/views.py
@@ -2,13 +2,13 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-  return render(request, 'pages/index.html')
+  return render(request, 'pages/index.html.jinja')
 
 def about(request):
-  return render(request, 'pages/about.html')
+  return render(request, 'pages/about.html.jinja')
 
 def contact(request):
-  return render(request, 'pages/contact.html')
+  return render(request, 'pages/contact.html.jinja')
 
 def error404(request, exception=None):
-  return render(request, 'errors/404.html', {'error': exception}, status=404)
+  return render(request, 'errors/404.html.jinja', {'error': exception}, status=404)
